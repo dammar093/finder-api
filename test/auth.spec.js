6bc222ef76084739c57db15053c9f854e1424884
dammar093 <dammarrana093@gmail.com>
2025-01-19 20:25:34 +0545

    added comment

--- test/auth.spec.js
+++ test/auth.spec.js
@@ -7,7 +7,10 @@ const email = "dammarrana098@gmail.com";
 const password = "@dyams093";
 const BASE_URL = "http://localhost:3000/api/v1";
 
+//**  interation testing for authentication **//
+
 describe("Authentication", () => {
+  //** user registartion api  testing with fullName,email,phoneNumber and passoword **//
   test("User registration", async () => {
     try {
       const response = await axios.post(`${BASE_URL}/users`, { fullName, email, phoneNumber, password });
@@ -18,6 +21,8 @@ describe("Authentication", () => {
     }
   });
 
+  //** user registartion api  testing with exist email and phoneNumber  fullName,email,phoneNumber and passoword **//
+
   test("User registration with existing email or phone", async () => {
     try {
       const response = await axios.post(`${BASE_URL}/users`, { fullName, email, phoneNumber, password });
@@ -27,12 +32,23 @@ describe("Authentication", () => {
     }
   });
 
+  //** user login api  testing with email and  passoword **//
   test("User login with email and password", async () => {
     try {
-      const response = await axios.post(`${BASE_URL}/users`, { email, password });
+      const response = await axios.post(`${BASE_URL}/users`, { fullName, email, password });
       expect(response.status).toBe(200);
     } catch (error) {
       expect(error.response?.status).toBe(400);
     }
   })
+
+  //** Fogort password  send otp to the email api  testing  **//
+
+
+  //** Verify otp api testing **//
+
+
+  //** reset password api testing **//
+
+
 });
