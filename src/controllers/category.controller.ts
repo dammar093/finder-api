7f2147ae67da1055d8c45de5628dc40ff848e7b6
dammar093 <dammarrana093@gmail.com>
2025-01-22 11:58:23 +0545

    added update category controller

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import asyncHandler from "../utils/asyncHandler";
 import ApiError from "../utils/errorHandler";
-import { createCategoryService } from "../services/category.service";
+import { createCategoryService, updateCategoryService } from "../services/category.service";
 import ApiResponse from "../utils/apiResponse";
 
 export const createCategory = asyncHandler(async (req: Request, res: Response) => {
@@ -15,4 +15,17 @@ export const createCategory = asyncHandler(async (req: Request, res: Response) =
     throw new ApiError(400, "Category alreay exist with this name");
   }
   return res.status(201).json(new ApiResponse(201, category, "Category created successfully"));
+});
+
+export const updateCategory = asyncHandler(async (req: Request, res: Response) => {
+  const { isActive } = req.body;
+  const { id } = req.params;
+  if (!id) {
+    throw new ApiError(400, "Id is required");
+  }
+  const category = await updateCategoryService(id, isActive);
+  if (!category) {
+    throw new ApiError(400, "Category not found with this id");
+  }
+  return res.status(200).json(new ApiResponse(200, category, "Category updated successfully"));
 });
