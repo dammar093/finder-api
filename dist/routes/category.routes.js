713d85022a1de6adb3d4e2252163ea398dd2ecfb
dammar093 <dammarrana093@gmail.com>
2025-01-22 11:51:19 +0545

    added create category routes

---
+++ dist/routes/category.routes.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const auth_1 = __importDefault(require("../utils/auth"));
+const category_controller_1 = require("../controllers/category.controller");
+const router = (0, express_1.Router)();
+router.post("/", auth_1.default, category_controller_1.createCategory);
+exports.default = router;
