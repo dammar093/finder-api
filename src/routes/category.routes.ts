315d9fb53900c247b6f09af346b3ae071c6198a2
dammar093 <dammarrana093@gmail.com>
2025-01-22 12:05:36 +0545

    fix get category route

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import verifyJwtToken from "../utils/auth";
-import { createCategory, updateCategory } from "../controllers/category.controller";
+import { createCategory, getCategories, updateCategory } from "../controllers/category.controller";
 
 const router = Router();
 router.post("/", verifyJwtToken, createCategory);
 router.patch("/:id", verifyJwtToken, updateCategory);
-router.get("/", verifyJwtToken, updateCategory);
+router.get("/", verifyJwtToken, getCategories);
 
 export default router;
